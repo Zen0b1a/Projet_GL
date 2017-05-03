1d89a3772fc8ea88dce1883ca7131590da543f8d
Annabelle GILLET <ag092850@ghostiem.iem>
2017-05-03 16:50:10 +0200

    COMMIT

--- GL/src/main/java/fr/univ/projet/gl/service/FilmService.java
+++ GL/src/main/java/fr/univ/projet/gl/service/FilmService.java
@@ -1,6 +1,10 @@
 package fr.univ.projet.gl.service;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.sql.rowset.CachedRowSet;
 
 import org.w3c.dom.*;
 
@@ -26,27 +30,74 @@ public class FilmService {
 	{
 		List<String[]> enregistrement = new ArrayList<>();
 		List<String> noms_colonnes = new ArrayList<>();
-		
+		//Lecture de Document xml
+		Element root = this.xml.getDocumentElement();
+        NodeList nodes = root.getChildNodes();
+        int nbNode = nodes.getLength();
+        for(int i=0; i<nbNode; i++)
+        {
+        	System.out.println("Nouveau film");
+        	Node n = nodes.item(i);
+        	Element element = (Element)n;
+        	NodeList film = element.getChildNodes();
+        	String[] film_a_enregistrer = new String[film.getLength()];
+        	for(int j=0; j<film.getLength(); j++)
+        	{
+        		Node attribut = film.item(j);
+        		if(!noms_colonnes.contains(attribut.getNodeName()))
+        		{
+        			noms_colonnes.add(attribut.getNodeName());
+        			System.out.println("Nom colonne : "+attribut.getNodeName());
+        		}
+        		film_a_enregistrer[j] = attribut.getTextContent();
+        		System.out.println("Valeur : "+attribut.getTextContent());
+        	}
+        	enregistrement.add(film_a_enregistrer);
+        }
 		//Enregistrement
-		FilmDAO filmDAO = new FilmDAO();
-		filmDAO.enregistrer(noms_colonnes, enregistrement);
-	}
-	
-	public void extraire()
-	{
-		
+		//FilmDAO filmDAO = new FilmDAO();
+		//filmDAO.enregistrer(noms_colonnes, enregistrement);
 	}
 	
 	/*
-	 * Lecture du fichier XML
+	 * Exctraction des données reçues de la base de données dans Document xml
+	 */
+	public void extraire()
+	{
+		FilmDAO filmDAO = new FilmDAO();
+		CachedRowSet crs = filmDAO.recuperer();
+		//Lecture du crs pour stocker les données dans Document xml
+		try 
+		{
+			ResultSetMetaData metaData = crs.getMetaData();
+			crs.beforeFirst();
+			while(crs.next())
+			{
+				for(int i=1; i<=metaData.getColumnCount(); i++)
+				{
+					
+				}
+			}
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * Lecture du fichier XML dans Document xml
 	 */
 	public void lireFichier()
 	{
 		this.xml = FileUtils.lire();
 	}
 	
+	/*
+	 * Enregistrement dans le fichier xml
+	 */
 	public void ecrire()
 	{
-		
+		FileUtils.ecrire(xml);
 	}
 }
